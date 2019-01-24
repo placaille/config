c703a4d58b1ca5dcefa3f53192bf21f174472983
Philippe Lacaille <philippe.lacaille@gmail.com>
2019-01-24 16:34:21 -0500

    make highlight fish grey

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,7 @@ source /Users/phil/miniconda3/etc/fish/conf.d/conda.fish
 
 # activate base env
 conda activate base
+
+# change highlight of tab-completion to dark-grey
+set -U fish_color_search_match --background='696969'
+
