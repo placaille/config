6b2f6d8d89c6b223285027cef3b2d61f6c7ea29d
Philippe Lacaille <placaille@users.noreply.github.com>
2021-12-11 16:27:33 -0500

    Delete fish directory

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-# miniconda
-source /Users/phil/miniconda3/etc/fish/conf.d/conda.fish
-
-# activate base env
-conda activate base
-
-# change highlight of tab-completion to dark-grey
-set -U fish_color_search_match --background='696969'
-
